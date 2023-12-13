d7aadf0d554801b3d9f98b069351d3d8c740c153
Plaxie <plaxie.py@gmail.com>
2023-12-13 07:54:01 +0300

    Update x_or_o.py
    removed the unnecessary need to even ask for turn of either X or O and added more functions.

--- XorO/x_or_o.py
+++ XorO/x_or_o.py
@@ -15,38 +15,43 @@ table = [
     ]
 ]
 
-pieces = [O, X]
+positions = [O, X]
 
-def interpretor(string: str, turn: int) -> Table:
+def interpretor(string: str, current_turn: int) -> tuple[bool, str | tuple[int]]:
 
     string = "".join(string.split())  # Removes any spaces if given.
 
-    if turnfind(string[0]) != turn:
-        return False, f"Not {pieces[abs(turn-1)]}'s Turn!"
+    try:
+        row, column = int(string[1]), int(string[2])
+    except ValueError:
+        return False, f"Move [{string[1]}][{string[2]}] are Invalid!"
+
+    if valid_move(row, column):
+        return True, (row, column)
+
     else:
-        try:
-            row, column = int(string[1]), int(string[2])
-        except ValueError:
-            return False, f"Move [{string[1]}][{string[2]}] are Invalid!"
+        return False, f"Move [{row}][{column}] are Invalid!"
 
-        if valid(row, column):
-            pass
-
-        else:
-            return False, f"Move [{row}][{column}] are Invalid!"
-
-def turnfind(character: str) -> int:
-    match character:
-        case 'X': return 1
-        case 'O': return 0
-    return 2
-
-def valid(*args):
+def valid_move(*args) -> bool:
     for move in args:
         if move not in [0,1,2]:
             return False
     else:
         return True
 
+def valid_position(row: int, column: int, table: Table) -> bool:
+    
+    if table[row][column] == empty:
+        return True
+    return False
+
+def placer(current_turn: int, coords: tuple[int], table: Table) -> Table:
+
+    character = positions[abs(current_turn)]
+    ro, co = coords
+    
+    if valid_position(ro, co, table):
+        table[ro][co] = character
 
 
+    
