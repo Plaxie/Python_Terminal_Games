0e1f64bda80305e5bfff4065b9c4ce0e9800ed75
Plaxie <plaxie.py@gmail.com>
2023-12-18 02:02:48 +0300

    Added comments and made changes for accurate designing.

--- SOS/sos.py
+++ SOS/sos.py
@@ -3,68 +3,74 @@ print('\033c')
 
 title = 'Main Menu'
 
-options = ['Start', 'Exit', 'Testing for spacing']
+options = ['Start', 'Exit']
 
-creds = 'By PLAXIE'
+ver= 'v1.0'
 
-version= '1.0'
+def display_menu(options: list|tuple, title: str = '', credit: str = '', version:str = ver, size:int = 20, divider:int = 1) -> None:
 
-def display_menu(options: list|tuple, title: str = '', credit: str = '', version:str = '', size:int = 20, divider:int = 1) -> None:
+    '''Displays a Menu or Page in a fixed format that contains: Title, Options, Credit and version with:-
+    - sections: options [required], title, credit, version
+    - elements: size (page size), divider (space between sections)'''
 
+    # alligned numbers for each option
     options = [f'[{idx}] {opt}' for idx, opt in enumerate(options, start=1)]
+    
+    # longest string from all the elements
+    longest_complete = (sorted([version + '   ' + credit] + [title] + options, key=len))[-1]
+    # longest string in the options for center allignment
+    longest_options = (sorted(options, key=len))[-1]
 
-    longest = (sorted([credit] + [title] + options, key=len))[-1]
-
-    min_display_width = len(longest) + 2
-
+    # the minimum size that is needed to fit all the elements.
+    min_display_width = len(longest_complete) + 2
+    # the display page size, if less than the minimum display width, then modify.
     size = min_display_width if size < min_display_width else size
 
+    # a design element used to divide different sections.
     true_divider = f'o{'':-^{size}}o'
+    # a design element used to give space inside sections.
+    sub_divider = '\n'.join([f'|{'': ^{size}}|'] * divider if divider > 0 else '')
 
-    sub_divider = f'|{'': ^{size}}|' * divider if divider > 0 else ''
-
-    
-
-
-    Title = '\n'.join(
+    # The TITLE ELEMENT:
+    Title = ''.join( #this\nthat\nthere + \n
         [
-            true_divider,
-            f'|{title: ^{size}}|',
+            true_divider  + '\n',
+            f'|{title: ^{size}}|' + '\n', 
             true_divider + '\n'
         ]
     )
     
-    space = f'|{longest: ^{size}}|'.find('[') - 1
+    # space needed before an option.
+    space = f'|{longest_options: ^{size}}|'.find('[') - 1
+    # calculate the length for correction.
     len_diff = len(" "*space)
 
-    Options = '\n'.join(
+    # The OPTIONS ELEMENT:
+    Options = ''.join( #this\nthat\nthere + \n
         [
-            sub_divider,
-            '\n'.join(
-                [
-                    f'|{" "*space}{opt: <{size-len_diff}}|' for opt in options
-                ]
+            sub_divider + '\n' if sub_divider != '' else sub_divider,
+            ('\n'.join(
+                [  # each option inside the list.
+                    f'|{" " * space}{opt: <{size-len_diff}}|' for opt in options
+                ]) + '\n'
             ),
-            sub_divider + '\n'
+            sub_divider + '\n' if sub_divider != '' else sub_divider
         ]
     )
-
-    Credit = '\n'.join(
+    # The LAST ELEMENT:
+    Last = ''.join( 
         [
-            sub_divider,
-            f'|  v{version}{credit: >{size - len(version) - 5}}  |',
-            sub_divider,
+            f'|  {version} {credit: >{size - len(version) - 5}}  |' + '\n',
+            sub_divider + '\n' if sub_divider != '' else sub_divider,
             true_divider + '\n'
         ]
     )
 
-    print(Title, Options, Credit, sep ='')
+    # Combine the Pieces.
+    Display = Title + Options + Last
+    # Display the page.
+    print(Display)
 
-    
-
-
-
-display_menu(options, title, creds, version)
 
 
 
