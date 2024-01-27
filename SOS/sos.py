86a81c17a2d680d81a303548971a8abb74d8ed67
Plaxie <plaxie.py@gmail.com>
2024-01-26 18:35:00 -0600

    update

--- SOS/sos.py
+++ SOS/sos.py
@@ -1,14 +1,19 @@
 
+from pyclbr import Function
+from types import FunctionType
+
+
 print('\033c')
-
-title = 'Main Menu'
-
-options = ['Start', 'Exit']
 
 ver= 'v1.0'
 
+#############################################
+#       GRAPHICS AND INPUT FUNCTIONS        #
+#############################################
 
-def display_menu(options: list|tuple, title: str = '', credit: str = '', version:str = ver, bottomborder: bool = True, size:int = 20, divider:int = 1) -> None:
+
+
+def display_menu(options: list|tuple, title: str = '', credit: str = '', version:str = ver, bottomborder: bool = True, clear = False, size:int = 20, divider:int = 1) -> None:
 
     '''Displays a Menu or Page in a fixed format that contains: Title, Options, Credit and version with:-
     - sections: options [required], title, credit, version
@@ -70,15 +75,39 @@ def display_menu(options: list|tuple, title: str = '', credit: str = '', version
     # Combine the Pieces.
     Display = Title + Options + Last
     # Display the page.
-    print(Display)
+    print('\033c' + Display if clear else Display)
 
 
-def get_input(var_names: list|tuple, type_list: list|tuple, inside: list|tuple = [], size: int = 20) -> tuple:
+def get_input(
+        display_func: Function,
+        var_names: list|tuple,
+        type_list: list|tuple,
+        inside: list|tuple = [],
+        size: int = 20, 
+        argsforfunc = (None,)
+        ) -> list|tuple:
 
     '''Get's Input of specified variables with correct types in a fixed format of size:
     - Variable Names [list of strings]
     - Types [list of strings] (must be equal length as var_names)
-    - Size [int] (Format page size)'''
+    - Inside [list of types that must be in the input for specific inputs]
+    - Size [int] (Format page size)
+    - Clear: To clear the terminal
+    - function to start with (usually a display page: Must provide all arguments)''' 
+
+    func = False
+
+    if display_func != None:
+        try: 
+            f_o, f_t, f_c, f_v, f_bb, f_cl, f_s, f_d = argsforfunc
+            Dispfc = display_func
+            func = True
+            
+        except ValueError:
+            print('Not enough argumants for func to operate')
+        
+
+
 
     # check if the provided type list has valid types.
     for types in type_list:
@@ -96,9 +125,11 @@ def get_input(var_names: list|tuple, type_list: list|tuple, inside: list|tuple =
         # while the inputs are incorrect retry for input.
         while True:
             try:
+                if func: Dispfc(f_o, f_t, f_c, f_v, f_bb, f_cl, f_s, f_d) # unbound: ignore
                 match type_list[idx]:  # Check if type of the current variable is...
                     case 'int':  # then try to convert to int, if error try again, else break
                         out = int(input(inputformat('Integer', message, size)))
+                        if out == '0': out = 0
                         if out not in inside:  
                             raise ValueError()  # Raise a value error for an invalid input.
                         else:
@@ -134,6 +165,8 @@ def get_input(var_names: list|tuple, type_list: list|tuple, inside: list|tuple =
     return results
 
 
+
+
 def inputformat(input_hint: str, message: str, size = 20) -> str:
 
     '''Formats for input:
@@ -147,11 +180,14 @@ def inputformat(input_hint: str, message: str, size = 20) -> str:
     temp_sentence = ''
 
     # for every word in the sentence ...
-    for word in stringtoformat.split():
+    for word in stringtoformat.split(sep = ' '):
 
-        # check if adding the fword would result in the breaking of the page,
+        # check if adding the word would result in the breaking of the page,
         # if not then add the word to the currrent string.
-        if len(temp_sentence + word + ' ') < size - 2: temp_sentence += word + ' '
+        if word == 'n':
+            stringparts += [temp_sentence]
+            temp_sentence = ''
+        elif len(temp_sentence + word + ' ') < size - 2: temp_sentence += word + ' '
         else: 
             # if yes then append the current sentence to the list of strings.
             stringparts += [temp_sentence]
@@ -175,7 +211,6 @@ def inputformat(input_hint: str, message: str, size = 20) -> str:
         ]
     )
 
-
     return page
 
 
@@ -189,8 +224,65 @@ messages = [
 types = ['int', 'str', 'bool', 'float']
 
 
-display_menu(options, title, 'By Plaxie', ver, False, 30)
-choice = get_input([
-    'Enter the option of choice testing for longer formatting'
-], ['int'], [1,2], 30
-)
+# display_menu(options, title, 'By Plaxie', ver, False, 30)
+# choice = get_input(
+#     display_menu, # type: ignore
+
+#     [
+#         'Enter the option choice'
+#     ], ['int'], [1,2], 30,
+
+#     (options, title, 'By Plaxie', ver, False, True, 30, 1)
+# )
+
+#############################################
+#            MAINLOOP FUNCTIONS             #
+#############################################
+
+def main_menu():
+
+    choice = get_input(
+        display_menu, # type: ignore
+        [
+            'Enter the option choice'
+        ], ['int'], [1,2], 30, 
+
+        (['Start', 'Exit'], 'Main Menu', 'By Plaxie', ver, False, True, 30, 1)
+    )
+
+    if choice == [2]: exit()
+
+    choice = get_input(
+        display_menu, # type: ignore
+        [
+            'Enter the option choice'
+        ], ['int'], [1,2], 30, 
+
+        (['3x3', '4x4', '5x5', '10x10', 'Custom!'], 'Size', 'By Plaxie', ver, False, True, 30, 1)
+    )
+
+
+#############################################
+#              LOGIC FUNCTIONS              #
+#############################################
+
+
+
+
+#############################################
+#              TABLE FUNCTIONS              #
+#############################################
+
+def display_table(table: list[list], title: str = '', clear:bool = False) -> None: 
+    
+    '''{Displays a Table} in a clean format:
+    - Table
+    - Title (not required)
+    - Clear: clears the terminal if set to [True]'''
+    
+    # Just string joins and list Comprehension Magic.
+    print('\n'.join(['\033c' if clear else ''] + [f'' if title == '' else '\n'.join([f'o{"":-^{6*len(table)-1}}o', f'|'+ f"{title: ^{6*len(table)-1}}" +'|'])]+ ['o-----'*len(table)+'o'] + [''.join([f'|#####' if row[cdx] == 0 else f'|{row[cdx]: ^5}' for cdx in range(len(row)) ] + ['|\n' + 'o-----'*len(table)+'o'])for rdx, row in enumerate(table)]))
+
+
+if __name__ == '__main__':
+    main_menu()
